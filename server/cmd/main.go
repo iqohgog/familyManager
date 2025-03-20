d26ed767909bbf8dded5d02f517559e578c0dc04
no_talk <toudr2378@gmail.com>
2025-03-20 17:14:54 +0300

    docker-work

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"fmt"
+	"v1/familyManager/configs"
+)
+
+// func App() http.Handler {
+
+// }
+
 func main() {
-    
+	conf := configs.LoadConfig()
+	fmt.Println(conf)
 }
