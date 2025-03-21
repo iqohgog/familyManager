3d96dfbe99ca2a9914820f3dc606e4f8415788a7
no_talk <toudr2378@gmail.com>
2025-03-21 18:29:14 +0300

    end of middleware and other

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -2,17 +2,33 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"os"
 	"v1/familyManager/configs"
 	"v1/familyManager/pkg/db"
+	"v1/familyManager/pkg/middleware"
 )
 
-// func App() http.Handler {
-
-// }
-
-func main() {
+func App() http.Handler {
 	conf := configs.LoadConfig()
-	fmt.Println(conf)
 	db := db.New(conf)
 	fmt.Println(db)
+
+	router := http.NewServeMux()
+	// authServices := auth.NewAuthService(userRepository)
+
+	stack := middleware.Chain(
+		middleware.CORS,
+	)
+	return stack(router)
+}
+
+func main() {
+	app := App()
+	server := http.Server{
+		Addr:    os.Getenv("SERVER_PORT"),
+		Handler: app,
+	}
+	fmt.Println("Server is lestining on port 8081")
+	server.ListenAndServe()
 }
